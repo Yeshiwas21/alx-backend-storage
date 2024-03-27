06b48b156d0f517c70737fd2f509ee3f3ec57b64
Yeshiwas Dagnaw <yeshiwasdagnaw@gmail.com>
2024-03-27 14:38:31 +0300

    fixed

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -11,17 +11,16 @@ if __name__ == "__main__":
     total_logs = nginx_collection.count_documents({})
     print(f"{total_logs} logs")
 
-    if total_logs == 0:
-        print("Methods:")
-        for method in ["GET", "POST", "PUT", "PATCH", "DELETE"]:
-            print(f"\tmethod {method}: 0")
-        print("0 status check")
-    else:
-        methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-        print("Methods:")
-        for method in methods:
-            count = nginx_collection.count_documents({"method": method})
-            print(f"\tmethod {method}: {count}")
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    method_counts = {method: 0 for method in methods}
 
-        status_check_count = nginx_collection.count_documents({"method": "GET", "path": "/status"})
-        print(f"{status_check_count} status check")
+    if total_logs > 0:
+        for method in methods:
+            method_counts[method] = nginx_collection.count_documents({"method": method})
+
+    print("Methods:")
+    for method, count in method_counts.items():
+        print(f"\tmethod {method}: {count}")
+
+    status_check_count = nginx_collection.count_documents({"method": "GET", "path": "/status"})
+    print(f"{status_check_count} status check")
