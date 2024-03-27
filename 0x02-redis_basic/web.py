515dd2cc0155e6deb728469fea7d3173e9a801e3
Yeshiwas Dagnaw <yeshiwasdagnaw@gmail.com>
2024-03-27 14:19:07 +0300

    all

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,46 +1,43 @@
 #!/usr/bin/env python3
 """Implementing an expiring web cache and tracker"""
+
 import requests
 import redis
-from functools import wraps
-from typing import Callable
 import functools
-
+from typing import Callable
 
 _redis = redis.Redis()
 
 
 def count_request(method: Callable) -> Callable:
-    """Count number of requests sent to a URL"""
+    """Decorator to count number of requests sent to a URL"""
 
-    @wraps(method)
-    def wrapper(*args, **kwargs):
+    @functools.wraps(method)
+    def wrapper(url: str) -> str:
         """Wrapper function for decorator"""
-        url = args[0]
         _redis.incr(f"count:{url}")
-        cache = _redis.get(f"count:{url}")
+        cache = _redis.get(f"cache:{url}")
 
         if cache:
             return cache.decode('utf-8')
         else:
             html = method(url)
-            _redis.setex(f"count:{url}", 10, html)
-        return html
+            _redis.setex(f"cache:{url}", 10, html)
+            return html
 
     return wrapper
 
 
 @count_request
 def get_page(url: str) -> str:
-    """Obtain HTML content through URL"""
+    """Function to obtain HTML content through URL"""
     res = requests.get(url)
     return res.text
 
 
-# Use functools.lru_cache to cache with expiration time of 10 seconds
-get_page = functools.lru_cache(maxsize=1, typed=False, timeout=10)(get_page)
-
-url = "http://slowwly.robertomurray.co.uk/delay/3000/url/http://www.example.com"
-print(get_page(url))  # First request, should fetch from server
-print(get_page(url))  # Second request, should fetch from cache
-print(get_page(url))  # Third request, should fetch from cache
+if __name__ == "__main__":
+    # Example usage
+    url = "http://slowwly.robertomurray.co.uk/delay/3000/url/http://www.example.com"
+    print(get_page(url))  # First request, should fetch from server
+    print(get_page(url))  # Second request, should fetch from cache
+    print(get_page(url))  # Third request, should fetch from cache
